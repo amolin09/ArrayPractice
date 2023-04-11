d54de25ed44b8a2c4004b3a8883db0546eaf02a2
amolin09 <alex.molina5829@gmail.com>
2023-04-10 21:35:36 -0500

    initial push

---
+++ main.js
@@ -0,0 +1,6 @@
+let cars = ['Ford', 'Chevy', 'Toyota', 'Hyundai']
+console.log(cars.length)
+
+let moreCars = ['Kia', 'Nissan', 'Jeep', 'Honda']
+let totalCars = cars.concat(moreCars)
+console.log(totalCars)
